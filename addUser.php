7724b74edf4721dbcbce1dee1045555033a786e0
eric <eric.holmqvist@skola.taby.se>
2022-01-29 12:32:26 +0100

    Användare funktionerna är nu klara

--- addUser.php
+++ addUser.php
@@ -7,25 +7,35 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 $sql = "SELECT * FROM `user`;";
 $result = $conn->query($sql);
 
-$ranNum=0;
 $userN=$_POST["name"];
 $passW=$_POST["pass"];
 
+#Kollar om produkten finns eller inte
+$namnFinnsRedan = false;
+
+#Går genom alla rader i databasen tills den hittar något som är likadant som posten
 while($row = $result->fetch_assoc()) {
     if($row["name"] == $userN) {
-        $ranNum=1;
-    } else {
-        $sql = "INSERT INTO user (name, pass) VALUES ('$userN', '$passW')";
-        $ranNum=2;
+        $namnFinnsRedan = true;
+        break;
     }
 }
-if ($ranNum=1){
-    echo("Användarnamnet " . $userN . " är redan användt");
-} elseif($ranNum=2){
-    echo("Användarnamnet " . $userN . " och lösenordet är registrerat");
+
+#Om namnet inte finns så insertas användar namnet och lösenordet i databasen, annars säger programmet att användarnamnet finns redan
+if ($namnFinnsRedan){
+    echo("Användarnamnet " . $userN . " är redan använt");
 } else{
-    echo("error");
+    $sql = "INSERT INTO user (name, pass) VALUES ('$userN', '$passW')";
+    if ($conn->query($sql) === TRUE) {
+        echo("Användarnamnet " . $userN . " och lösenordet är registrerat");
+    } else {
+        echo "Något gick fel, försök igen" . "<br>" . $conn->error;
+    }
 }
+
+  
+$conn->close();
+
 ?>
 
 <button type="button" onclick="window.location.href='index.html'">Gå tillbaka</button>

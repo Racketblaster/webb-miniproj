a339cc7b1c163b0a4f7e40e054ecdc1fad70a7ec
eric <eric.holmqvist@skola.taby.se>
2022-03-12 13:51:43 +0100

    Kan nu ändra lösenord

---
+++ changePass.php
@@ -0,0 +1,54 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "webb-miniproj";
+$conn = mysqli_connect($servername, $username, $password, $database);
+$sql = "SELECT * FROM `user`;";
+$result = $conn->query($sql);
+
+$userN=$_POST["name"];
+$passW=$_POST["pass"];
+$nPassW=$_POST["newPass"];
+
+#Kollar om produkten finns eller inte
+$rättAnvändare = false;
+$felLösenord = false;
+
+#Försök på att fixa om man inte skriver in något som användar namn eller lösenord
+/*if (empty($userN or $passW)){
+    echo("Du glömde skriva in något användarnamn eller lösenord. Försök igen");
+}*/
+
+#Går genom alla rader i databasen tills den hittar något som är likadant som posten
+while($row = $result->fetch_assoc()) {
+    if(($row["name"] == $userN) && ($row["pass"] == $passW)){
+        $rättAnvändare = true;
+        break;
+    }
+    /*
+    elseif($row["name"] == $userN && !($row["pass"] == $passW)) {
+        $felLösenord = true;
+        break;
+    } 
+    */
+}
+
+#Om namnet inte finns så insertas användar namnet och lösenordet i databasen, annars säger programmet att användarnamnet finns redan
+if (!$rättAnvändare){
+    echo("Det finns ingen användare med namnet: " . $userN);
+} else{
+    $sql = "update user SET pass='$nPassW' WHERE name='$userN'";
+    if ($conn->query($sql) === TRUE) {
+        echo("Lösenordet har ändrats. ");
+    } else {
+        echo "Något gick fel, försök igen" . "<br>" . $conn->error;
+    }
+}
+
+  
+$conn->close();
+
+?>
+
+<button type="button" onclick="window.location.href='index.html'">Gå tillbaka</button>
